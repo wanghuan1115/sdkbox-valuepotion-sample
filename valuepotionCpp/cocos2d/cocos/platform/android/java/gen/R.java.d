64f960071f7debc4b62f23a8f9a508bd81552e4e
hugo <hugohuang1111@gmail.com>
2015-10-14 16:59:02 +0800

    add ignore file

---
+++ valuepotionCpp/cocos2d/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/htl/Documents/Work/sdkbox-valuepotion-sample/valuepotionCpp/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
---
+++ valuepotionCpp/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java.d
@@ -0,0 +1,3 @@
+/Users/htl/Documents/Work/sdkbox-valuepotion-sample/valuepotionCpp/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java: \
+  /Users/htl/Documents/Work/sdkbox-valuepotion-sample/valuepotionCpp/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl 
+
