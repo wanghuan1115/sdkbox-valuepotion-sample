c30c5ef921a8b29f6bc003dd226171ab19b970e9
Nite Luo <darkdukey@gmail.com>
2015-10-28 14:56:37 -0700

    Fix cpp android build

--- valuepotionCpp/cocos2d/cocos/platform/android/java/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/htl/Documents/Work/sdkbox-valuepotion-sample/valuepotionCpp/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
--- valuepotionCpp/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java.d
+++
@@ -1,3 +0,0 @@
-/Users/htl/Documents/Work/sdkbox-valuepotion-sample/valuepotionCpp/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java: \
-  /Users/htl/Documents/Work/sdkbox-valuepotion-sample/valuepotionCpp/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl 
-
